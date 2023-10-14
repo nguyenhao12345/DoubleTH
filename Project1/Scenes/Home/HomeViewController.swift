b270c343ae81a37ba4c90659123a6c1d341f2cfd
NguyenHao <anhhaonguyen.st@gmail.com>
2023-10-14 09:36:46 +0700

    - remove network

--- Project1/Scenes/Home/HomeViewController.swift
+++ Project1/Scenes/Home/HomeViewController.swift
@@ -12,12 +12,12 @@ var arrayImagesGlobal: [String: UIImage] = [:]
 final class HomeViewController: BaseViewController {
     @IBOutlet private(set) var albumCollectionView: BaseCollectionView!
     
-    private(set) var arrayAlbumModel: [AlbumModel] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        testRequest()
+        
     }
     
     override func setupView() {
@@ -33,58 +33,14 @@ private extension HomeViewController {
     }
 }
 
-//MARK: request API
-extension HomeViewController: SendingAPIRequestInteractionsAble {
-     private func testRequest() {
-        self.sendRequest(.init(endpoint: .test, method: .get)) { [weak self] result in
-            guard let self = self else { return }
-            switch result {
-            case .success(let data):
-                guard let data = data else { return }
-                do {
-                    if let array: [[String: Any]] = try data.parser() {
-                        self.loadArrayAlbumnOnSuccess(with: array)
-                    } else {
-                        
-                    }
-                } catch {
-                    break
-                }
-            case .failure(_):
-                break
-            }
-        }
-    }
-    
-    private func loadArrayAlbumnOnSuccess(with array: [[String: Any]]) {
-        arrayAlbumModel = array.map { .init(with: $0) }
-        albumCollectionView.reloadData()
-    }
-}
-
 extension HomeViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        arrayAlbumModel.count
+        10
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AlbumCollectionViewCell.identifier, for: indexPath) as? AlbumCollectionViewCell else { return UICollectionViewCell() }
-        if let albumAtIndexPath = arrayAlbumModel[safe: indexPath.row] {
-            cell.config(with: albumAtIndexPath.title, image: arrayImagesGlobal[albumAtIndexPath.thumbnailUrl] ?? UIImage())
-            
-//            if arrayImagesGlobal[albumAtIndexPath.thumbnailUrl] == nil {
-//                let operationQueue = OperationQueue()
-//                let operation = ImageDownloader(urlAsText: albumAtIndexPath.thumbnailUrl)
-//                operationQueue.addOperation(operation)
-//                operation.completionBlock = {
-//                    DispatchQueue.main.async {
-//                        collectionView.reloadItems(at: [indexPath])
-//                        print(indexPath)
-//                    }
-//                }
-//            }
-        }
-        
+
         return cell
     }
 }
