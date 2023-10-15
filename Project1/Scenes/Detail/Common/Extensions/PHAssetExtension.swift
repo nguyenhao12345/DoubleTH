690acbf359bc23b429886fdb34af5d25b47d1973
truongdev <>
2023-10-15 22:04:12 +0700

    change image target size

--- Project1/Scenes/Detail/Common/Extensions/PHAssetExtension.swift
+++ Project1/Scenes/Detail/Common/Extensions/PHAssetExtension.swift
@@ -14,7 +14,7 @@ public extension PHAsset {
         let option = PHImageRequestOptions()
         var thumbnail = UIImage()
         option.isSynchronous = true
-        manager.requestImage(for: self, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: option, resultHandler: {(result, info)->Void in
+        manager.requestImage(for: self, targetSize: PHImageManagerMaximumSize, contentMode: .aspectFit, options: option, resultHandler: {(result, info)->Void in
             thumbnail = result!
         })
         return thumbnail
